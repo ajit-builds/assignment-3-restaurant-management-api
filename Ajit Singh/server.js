30faf03b2f701553f89a686c1d653b4f7c15072b
Ajit  Singh <aartianilsingh02@gmail.com>
2026-08-21 10:42:26 +0530

    Add files via upload

--- Ajit Singh/server.js
+++ Ajit Singh/server.js
@@ -1,1 +1,16 @@
+require("dotenv").config();
+const express = require('express');
+const db = require("./config/db");
+const restaurantRouter = require("./router/restaurantRouter");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/restaurants", restaurantRouter);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
