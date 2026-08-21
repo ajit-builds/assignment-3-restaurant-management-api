e9021fb38883f862ef20bde15199282850064845
Ajit  Singh <aartianilsingh02@gmail.com>
2026-08-21 10:39:09 +0530

    Set up MongoDB connection with event handlers

--- Ajit Singh/config/db.js
+++ Ajit Singh/config/db.js
@@ -1,1 +1,19 @@
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGODB_URI);
+
+const db = mongoose.connection;
+
+db.on("connected", () => {
+    console.log("Mongodb is connected");
+});
+
+db.on("disconnected", () => {
+    console.log("Mongodb is disconnected");
+});
+
+db.on("error", (error) => {
+    console.log("Mongodb is disconnected due to error", error);
+});
+
+module.exports = db;
